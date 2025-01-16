7f1de03177c529670a35a8e64768c67e06275480
itsmeakhil <akhiledathadan007@gmail.com>
2025-01-16 22:32:58 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MyDevTools",
+  title: "MyDevTools - Essential tools for Developers",
   keywords: [
     "Developer Tools",
     "JSON Formatter",
