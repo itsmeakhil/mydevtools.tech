904a3174b74ea68b5a917fba1ec49d3bb149fb0d
ananducv <153972540+anandude@users.noreply.github.com>
2025-02-12 13:58:23 +0530

    add skeleton for todo items

--- src/app/app/to-do/TaskList.tsx
+++ src/app/app/to-do/TaskList.tsx
@@ -2,6 +2,8 @@
 "use client";
 
 import TaskItem from "./TaskItem";
+import TaskSkeleton from "./components/TaskSkeleton";
+import { FadeIn } from "@/components/ui/fade-in";
 
 interface Task {
   id: string;
@@ -12,21 +14,34 @@ interface Task {
 
 interface TaskListProps {
   tasks: Task[];
+  isLoading: boolean;
   onUpdateStatus: (id: string, status: "not-started" | "ongoing" | "completed") => void;
   onDeleteTask: (id: string) => void;
 }
 
-export default function TaskList({ tasks, onUpdateStatus, onDeleteTask }: TaskListProps) {
+export default function TaskList({ tasks, isLoading, onUpdateStatus, onDeleteTask }: TaskListProps) {
   return (
     <ul>
-      {tasks.map((task) => (
-        <TaskItem
-          key={task.id}
-          task={task}
-          onUpdateStatus={onUpdateStatus}
-          onDeleteTask={onDeleteTask}
-        />
-      ))}
+      <FadeIn show={isLoading}>
+        {isLoading && (
+          <div>
+            {[...Array(5)].map((_, index) => (
+              <TaskSkeleton key={index} />
+            ))}
+          </div>
+        )}
+      </FadeIn>
+      
+      <FadeIn show={!isLoading}>
+        {!isLoading && tasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            onUpdateStatus={onUpdateStatus}
+            onDeleteTask={onDeleteTask}
+          />
+        ))}
+      </FadeIn>
     </ul>
   );
 }
