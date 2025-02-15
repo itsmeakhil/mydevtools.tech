16badc6b626aba0ccac3d18127f2d22090dfe3d9
ananducv <153972540+anandude@users.noreply.github.com>
2025-02-15 09:03:11 +0530

    checking push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-// import { GeistSans, GeistMono } from 'geist/font';
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Toaster } from 'sonner'
