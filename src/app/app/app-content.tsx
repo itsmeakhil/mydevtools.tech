5d2ecbed2ac68156fa4cc01063b5a5e67c0e9c17
ananducv <153972540+anandude@users.noreply.github.com>
2025-02-17 09:18:49 +0530

    url encoder added

--- src/app/app/app-content.tsx
+++ src/app/app/app-content.tsx
@@ -10,8 +10,8 @@ export function AppContent({ children }: { children: React.ReactNode }) {
 
   return (
     <div style={{ "--sidebar-width": "16rem", "--sidebar-width-icon": "3rem" } as React.CSSProperties} 
-         className="group/sidebar-wrapper flex min-h-svh w-full has-[[data-variant=inset]]:bg-sidebar">
-      <div className="flex h-screen font-mono w-full">
+         className="group/sidebar-wrapper flex min-h-screen w-full has-[[data-variant=inset]]:bg-sidebar">
+      <div className="flex min-h-screen w-full">
         <aside className={`${state === 'collapsed' ? 'w-[var(--sidebar-width-icon)]' : 'w-[var(--sidebar-width)]'} ${state === 'collapsed' ? '' : 'border-r'} p-4 flex flex-col`}>
           <div className="flex-1">
             <AppSidebar />
