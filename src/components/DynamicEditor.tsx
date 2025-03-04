bbfbe89e36588b376d1af0a935bd621b66f9d6bd
ananducv <ananducv666@gmail.com>
2025-03-04 14:14:57 +0530

    deleted unwanted blocknote editor components

--- src/components/DynamicEditor.tsx
+++
@@ -1,9 +0,0 @@
-// components/DynamicEditor.tsx
-"use client";
-
-import dynamic from "next/dynamic";
-
-// Dynamically import the Editor component with SSR disabled
-const Editor = dynamic(() => import("./Editor"), { ssr: false });
-
-export default Editor;
