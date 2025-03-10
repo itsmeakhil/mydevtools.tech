93884897c92e1b9f31cdc9519d03a037b4a139be
shivilSR4 <shivilkravi>
2025-03-10 17:18:36 +0530

    Numeronym Generator module added

---
+++ src/app/app/numeronym-generator/page.tsx
@@ -0,0 +1,9 @@
+import NumeronymGenerator from "./Component/NumeronymGenerator";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <NumeronymGenerator />
+    </main>
+  );
+}
